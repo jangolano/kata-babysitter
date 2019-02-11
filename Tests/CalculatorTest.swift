35295db9b91e3bd74ae44bbe84e220424094ba16
Josh Angolano <grangolano@gmail.com>
2019-02-11 14:28:13 -0600

    Inital Commit Failing FamilyA before 11Am

---
+++ Tests/CalculatorTest.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorTest.swift
+//  Tests
+//
+//  Created by Josh Angolano on 2019-02-09.
+//  Copyright © 2019 jangolano. All rights reserved.
+//
+
+import XCTest
+
+class CalculatorTests: XCTestCase {
+    let calc = Calculator();
+    func testFamilyABefore11(){
+        XCTAssertEqual(calc.calculatFamilyABefore11(), 15)
+    }
+}
---
+++ babysitter-kata/Calculator.swift
@@ -0,0 +1,19 @@
+//
+//  Calculator.swift
+//  babysitter-kata
+//
+//  Created by Josh Angolano on 2019-02-09.
+//  Copyright © 2019 jangolano. All rights reserved.
+//
+
+import Foundation
+
+class Calculator{
+    init() {
+
+    }
+    func calculatFamilyABefore11()->Int{
+        return 1;
+    }
+
+}
