611f4166ce1969c4b848cb6849ba92a084f253a2
Josh Angolano <grangolano@gmail.com>
2019-02-17 13:25:17 -0600

    Additional Refactoring

--- Tests/CalculatorTest.swift
+++ Tests/CalculatorTest.swift
@@ -10,33 +10,6 @@ import XCTest
 
 class CalculatorTests: XCTestCase {
     let calc = Calculator();
-    func testFamilyABefore11(){
-        XCTAssertEqual(calc.calculatFamilyABefore11(), 15)
-    }
-
-    func testFamilyAAfter11(){
-        XCTAssertEqual(calc.calculatFamilyAAfter11(), 20)
-    }
-
-    func testFamilyBBefore10(){
-        XCTAssertEqual(calc.calculatFamilyBefore10(), 12)
-    }
-
-    func testFamilyBBetween10and12(){
-        XCTAssertEqual(calc.calculatFamilyBBetween10and12(), 8);
-    }
-
-    func testFamilyBAfter12(){
-        XCTAssertEqual(calc.calculateFamilyBAfter12(), 16)
-    }
-
-    func testFamilyCBefore9(){
-        XCTAssertEqual(calc.calculateFamilyCBefore9(), 21)
-    }
-
-    func testFamilyCAfter9(){
-        XCTAssertEqual(calc.calculateFamilyCAfter9(), 15)
-    }
 
     func testCalculateEndingHours(){
         let endingHours = calc.calculateEndingHours(startTime: 5, endTime: 8)
--- babysitter-kata/Calculator.swift
+++ babysitter-kata/Calculator.swift
@@ -19,52 +19,25 @@ class Calculator{
     init(){
         rates = Array<Rate>();
         rates.append(Rate(family: "A", endingTime: 11, rate: 15))
-        rates.append(Rate(family: "A", endingTime: 4, rate: 20))
+        rates.append(Rate(family: "A", endingTime: 16, rate: 20))
         rates.append(Rate(family: "B", endingTime: 10, rate: 12))
         rates.append(Rate(family: "B", endingTime: 12, rate: 8))
-        rates.append(Rate(family: "B", endingTime: 4, rate: 16))
+        rates.append(Rate(family: "B", endingTime: 16, rate: 16))
         rates.append(Rate(family: "C", endingTime: 9, rate: 21))
-        rates.append(Rate(family: "C", endingTime: 4, rate: 15))
+        rates.append(Rate(family: "C", endingTime: 16, rate: 15))
     }
     
-    func calculatFamilyABefore11()->Int{
-        return calculateHourlyRate(family: "A", endingTime: 11)
-    }
-
-    func calculatFamilyAAfter11()->Int{
-        return calculateHourlyRate(family: "A", endingTime: 4)
-    }
-
-    func calculatFamilyBefore10()->Int{
-        return calculateHourlyRate(family: "B", endingTime: 10)
-    }
-
-    func calculatFamilyBBetween10and12()->Int{
-        return calculateHourlyRate(family: "B", endingTime: 12)
-    }
-
-    func calculateFamilyBAfter12()->Int{
-        return calculateHourlyRate(family: "B", endingTime: 4)
-    }
-
-    func calculateFamilyCBefore9()->Int{
-        return calculateHourlyRate(family: "C", endingTime: 9)
-    }
-
-    func calculateFamilyCAfter9()->Int{
-        return calculateHourlyRate(family: "C", endingTime: 4)
-    }
-
     func calculateHourlyRate(family:Character, endingTime:Int)->Int{
         for Rate in rates{
             if(Rate.family==family){
-                if(Rate.endingTime==endingTime){
+                if(Rate.endingTime>=endingTime){
                     return Rate.rate;
                 }
             }
         }
         return 0;
     }
+    
     func calculateEndingHours(startTime:Int, endTime:Int)->Array<Int>{
         var results = Array<Int>();
         var completeTime = endTime;
@@ -76,35 +49,13 @@ class Calculator{
         }
         return results;
     }
-
+    
     func calculatePay(family:Character, startTime:Int, endTime:Int)->Int{
         var totalPay=0;
         let endingHours = calculateEndingHours(startTime: startTime, endTime: endTime)
         for hour in endingHours{
-            if(family=="A"){
-                if(hour<=11){
-                    totalPay+=calculatFamilyABefore11();
-                }else{
-                    totalPay+=calculatFamilyAAfter11();
-                }
-            }else if(family=="B"){
-                if(hour<=10){
-                    totalPay+=calculatFamilyBefore10();
-                }else if(hour<=12){
-                    totalPay+=calculatFamilyBBetween10and12()
-                }else{
-                    totalPay+=calculateFamilyBAfter12()
-                }
-            }else if(family=="C"){
-                if(hour<=9){
-                    totalPay+=calculateFamilyCBefore9()
-                }else{
-                    totalPay+=calculateFamilyCAfter9()
-                }
-            }
+            totalPay+=calculateHourlyRate(family: family, endingTime: hour)
         }
         return totalPay;
     }
-    
-    
 }
