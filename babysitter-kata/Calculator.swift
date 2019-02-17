7684a2784e6680c39bfbf584c89c5f65e8dac002
Josh Angolano <grangolano@gmail.com>
2019-02-17 13:15:46 -0600

    Refactored to use an array to hold the lookup values and rates

--- babysitter-kata/Calculator.swift
+++ babysitter-kata/Calculator.swift
@@ -9,37 +9,62 @@
 import Foundation
 
 class Calculator{
-    init() {
-
+    struct Rate {
+        var family:Character = " "
+        var endingTime:Int = 0
+        var rate:Int = 0
     }
+    var rates: Array<Rate>;
+
+    init(){
+        rates = Array<Rate>();
+        rates.append(Rate(family: "A", endingTime: 11, rate: 15))
+        rates.append(Rate(family: "A", endingTime: 4, rate: 20))
+        rates.append(Rate(family: "B", endingTime: 10, rate: 12))
+        rates.append(Rate(family: "B", endingTime: 12, rate: 8))
+        rates.append(Rate(family: "B", endingTime: 4, rate: 16))
+        rates.append(Rate(family: "C", endingTime: 9, rate: 21))
+        rates.append(Rate(family: "C", endingTime: 4, rate: 15))
+    }
+    
     func calculatFamilyABefore11()->Int{
-        return 15;
+        return calculateHourlyRate(family: "A", endingTime: 11)
     }
 
     func calculatFamilyAAfter11()->Int{
-        return 20;
+        return calculateHourlyRate(family: "A", endingTime: 4)
     }
 
     func calculatFamilyBefore10()->Int{
-        return 12;
+        return calculateHourlyRate(family: "B", endingTime: 10)
     }
 
     func calculatFamilyBBetween10and12()->Int{
-        return 8;
+        return calculateHourlyRate(family: "B", endingTime: 12)
     }
 
     func calculateFamilyBAfter12()->Int{
-        return 16;
+        return calculateHourlyRate(family: "B", endingTime: 4)
     }
 
     func calculateFamilyCBefore9()->Int{
-        return 21;
+        return calculateHourlyRate(family: "C", endingTime: 9)
     }
 
     func calculateFamilyCAfter9()->Int{
-        return 15;
+        return calculateHourlyRate(family: "C", endingTime: 4)
     }
 
+    func calculateHourlyRate(family:Character, endingTime:Int)->Int{
+        for Rate in rates{
+            if(Rate.family==family){
+                if(Rate.endingTime==endingTime){
+                    return Rate.rate;
+                }
+            }
+        }
+        return 0;
+    }
     func calculateEndingHours(startTime:Int, endTime:Int)->Array<Int>{
         var results = Array<Int>();
         var completeTime = endTime;
